d9c77cdf34b61c610f44df1e559f4dd502803efe
fuxintong <1064756588@qq.com>
2020-05-18 20:40:14 +0800

    初始化项目

---
+++ mymall-member/src/main/java/com/fxt/mymall/member/MymallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.fxt.mymall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallMemberApplication.class, args);
+    }
+
+}
---
+++ mymall-warehouse/src/main/java/com/fxt/mymall/warehouse/MymallWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.fxt.mymall.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallWarehouseApplication.class, args);
+    }
+
+}
